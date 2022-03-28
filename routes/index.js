8df5baad5e2949806d913d3fcd28c539d7316473
r0808 <arthurdelophem3@gmail.com>
2022-03-28 17:47:00 +0200

    feat: add live scoreboard to routes and add style

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'VRL Score updater' });
+});
+
+router.get('/live', (req, res) => {
+  res.render('live', { title: 'VRL Scoreboard' });
 });
 
 module.exports = router;
