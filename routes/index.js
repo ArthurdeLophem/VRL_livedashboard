2df2502cef09e26f78863c6e1b42c8f9f24a0b70
r0808 <arthurdelophem3@gmail.com>
2022-04-11 22:57:53 +0200

    fix: rename routes and jade for default view and update

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', { title: 'VRL Score updater' });
 });
 
-router.get('/live', (req, res) => {
-  res.render('live', { title: 'VRL Scoreboard' });
+router.get('/updatestats', (req, res, next) => {
+  res.render('updateStats', { title: 'VRL Scoreboard' });
 });
 
 module.exports = router;
